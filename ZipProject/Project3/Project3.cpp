061c1c8835eefd4125a7e315d3f2be90c5f4392f
ryanh6900 <68438637+ryanh6900@users.noreply.github.com>
2021-12-09 21:24:24 -0500

    one label away...

--- ZipProject/Project3/Project3.cpp
+++ ZipProject/Project3/Project3.cpp
@@ -33,7 +33,7 @@ int main()
 	
 	sf::RenderWindow window;
 	sf::Vector2i centerWindow((sf::VideoMode::getDesktopMode().width / 2) - 455, (sf::VideoMode::getDesktopMode().height / 2) - 480);
-	window.create(sf::VideoMode(900, 900), "US Trip Calculator", sf::Style::Titlebar | sf::Style::Close);
+	window.create(sf::VideoMode(900, 1000), "US Trip Calculator", sf::Style::Titlebar | sf::Style::Close);
 	window.setPosition(centerWindow);
 	window.setKeyRepeatEnabled(true);
 	/*::RenderWindow loadingScreen;
@@ -50,23 +50,23 @@ int main()
 	sf::RectangleShape rectA(sf::Vector2f(120, 20));
 	rectA.setSize(sf::Vector2f(400,20 )); //(textbox1.text.str().length() + 1) * 10
 	rectA.setFillColor(sf::Color(200,200, 200));
-	rectA.setPosition({ 300,110 });
+	rectA.setPosition({ 300,60 });
 
 	sf::RectangleShape rectB(sf::Vector2f(120, 20));
 	rectB.setSize(sf::Vector2f(400, 20)); //(textbox2.text.str().length() + 1) * 10
 	rectB.setFillColor(sf::Color(200,200,200));
-	rectB.setPosition({ 300,160 });
+	rectB.setPosition({300,110 });
 
 	Textbox textbox1(15, 5, sf::Color::Black,false,rectA);
 	textbox1.setFont(arial);
 	textbox1.setLimit(true, 50);
-	textbox1.setPosition({ 300,110 });
+	textbox1.setPosition({300,60 });
 	//mp[1] = textbox1;
 
 	Textbox textbox2(15, 5, sf::Color::Black, false,rectB);
 	textbox2.setFont(arial);
 	textbox2.setLimit(true, 50);
-	textbox2.setPosition({ 300,160 });
+	textbox2.setPosition({300,110 });
 	//mp[2] = textbox2;
 
 	
@@ -76,21 +76,28 @@ int main()
 	labelCityA.setString("City A: ");
 	labelCityA.setFont(arial);
 	labelCityA.setFillColor(sf::Color::Black);
-	labelCityA.setPosition({200,100 });
+	labelCityA.setPosition({200,50 });
 
 	labelCityB.setString("City B: ");
 	labelCityB.setFont(arial);
 	labelCityB.setFillColor(sf::Color::Black);
-	labelCityB.setPosition({200,150 });
+	labelCityB.setPosition({200,100 });
 	
+	sf::Text Note;
+	Note.setString("Note: All calculated total distances include the distance added by changing elevation.");
+	Note.setFont(arial);
+	Note.setFillColor(sf::Color::Black);
+	Note.setCharacterSize(20);
+	Note.setPosition(100, 400);
+
 	sf::Text loadText;
 	loadText.setString("Loading Data for all US Cities...");
 	loadText.setFont(arial);
 	loadText.setCharacterSize(60);
 	loadText.setFillColor(sf::Color::Black);
-	loadText.setPosition({ 50,100 });
+	loadText.setPosition({ 50,200 });
 
-	Button calcBtn("Calculate", { 250,85 }, 60, sf::Color::Blue, sf::Color::Black);
+	Button calcBtn("Calculate", { 250,85 }, 60, sf::Color::White, sf::Color::Black);
 	/*Button(std::string t, sf::Vector2f size, int charSize, sf::Color bgColor, sf::Color textColor) {
 		text.setString(t);
 		text.setFillColor(textColor);
@@ -99,13 +106,24 @@ int main()
 		button.setFillColor(bgColor);
 	}*/
 	calcBtn.setFont(arial);
-	calcBtn.setPosition({350,200});
+	calcBtn.setPosition({350,150});
 	
 
-	Button Solving("Solving...", { 250,85 }, 60, sf::Color::Green, sf::Color::Black);
+	Button Solving("Solving...", { 350,85 }, 60, sf::Color::Green, sf::Color::Black);
 	 //(textbox1.text.str().length() + 1) * 10
 	Solving.setFont(arial);
-	Solving.setPosition({350,200});
+	Solving.setPosition({350,150});
+
+
+	Button SolvingD("Solving Dijkstra...", {500,85 }, 60, sf::Color::Red, sf::Color::Black);
+	//(textbox1.text.str().length() + 1) * 10
+	SolvingD.setFont(arial);
+	SolvingD.setPosition({ 200,150 });
+
+	Button SolvingB("Solving Bellman Ford...", { 575,85 }, 60, sf::Color::Yellow, sf::Color::Black);
+	//(textbox1.text.str().length() + 1) * 10
+	SolvingB.setFont(arial);
+	SolvingB.setPosition({ 200,150 });
 
 	/*sf::Text SolveText;
 	SolveText.setString("Solving...");
@@ -117,24 +135,31 @@ int main()
 	bool firstTime = true;
 	bool currA,currB;
 	
-	sf::Text result;
-	result.setFont(arial);
-	result.setCharacterSize(35);
-	result.setFillColor(sf::Color::Black);
-	result.setPosition(150, 300);
+	sf::Text resultBellman;
+	resultBellman.setFont(arial);
+	resultBellman.setCharacterSize(28);
+	resultBellman.setFillColor(sf::Color::Black);
+	resultBellman.setPosition(75, 250);
+
+	sf::Text resultDijkstra;
+	resultDijkstra.setFont(arial);
+	resultDijkstra.setCharacterSize(28);
+	resultDijkstra.setFillColor(sf::Color::Black);
+	resultDijkstra.setPosition(500, 250);
 
 	while (window.isOpen()) {
 		sf::Event anEvent;
 		if(firstTime) {
 			sf::RectangleShape Loading(sf::Vector2f(120, 50));
 			
-			Loading.setSize({ 900,200 }); //(textbox1.text.str().length() + 1) * 10
+			Loading.setSize({900,200 }); //(textbox1.text.str().length() + 1) * 10
 			Loading.setFillColor(sf::Color::Yellow);
 			Loading.setPosition({ 0,0 });
-			Loading.move(0, 50);
+			Loading.move(0, 150);
 			window.clear(sf::Color(200, 255, 255));
 			window.draw(Loading);
 			window.draw(loadText);
+			window.draw(Note);
 			window.display();
 
 			
@@ -201,37 +226,50 @@ int main()
 					if (calcBtn.isMouseOver(window))
 						calcBtn.setBackColor(sf::Color::Cyan);
 					else
-						calcBtn.setBackColor(sf::Color::Blue);
+						calcBtn.setBackColor(sf::Color::White);
 					break;
 
 				case sf::Event::MouseButtonReleased:
 					if (calcBtn.isMouseOver(window)) {
 						currA = false;
 						currB = false;
-						std::string ans = "";
-						result.setString(ans);
-						vector<string> path;
+						std::string ansBellman = "Total Distance includes elevation\n";
+						//window.draw(resultBellman);
+						std::string ansDijkstra = "";
+						resultBellman.setString(ansBellman);
+						resultDijkstra.setString(ansDijkstra);
+						vector<string> pathBell;
+						vector<string> pathDijk;
 						Solving.drawTo(window);
-						
-						//window.draw(SolveText);
 						window.display();
+						//window.draw(SolveText);
+						
 						/*cout << _graph->BellmanFord(textbox1.getText(), textbox2.getText(), path);*/
-						ans = "Total Distance: " + to_string(_graph->BellmanFord(textbox1.getText(), textbox2.getText(), path)) + " miles" + "\n";
-						//ans +=  _graph->BellmanFord("Orlando FL", "Lansing MI", path) +"km";
-						ans += "Stops: \n";
-						for (int i = 0; i < path.size()-1; i++)
-							ans += path[i] + "\n";
+						
+						SolvingB.drawTo(window);
+						window.display();
+						ansBellman = "Total Distance: " + to_string(_graph->BellmanFord(textbox1.getText(), textbox2.getText(), pathBell)) + " miles" + "\n";
+						//ansBellman +=  _graph->BellmanFord("Orlando FL", "LansBellmaning MI", path) +"km";
+						ansBellman += "Stops: \n";
+						for (int i = 0; i < pathBell.size()-1; i++)
+							ansBellman += pathBell[i] + "\n";
+						SolvingD.drawTo(window);
+						window.display();
+						ansDijkstra = "Total Distance: " + to_string(_graph->dijkstra(textbox1.getText(), textbox2.getText(), pathDijk)) + " miles" + "\n";
+						ansDijkstra += "Stops: \n";
+						for (int i = 0; i < pathDijk.size() - 1; i++)
+							ansDijkstra += pathDijk[i] + "\n";
 						/*for (int i = 0; i < path.size()-1; i++)
 							cout<< path[i] << "\n";*/
 						//cout << _graph->dijkstra(textCityA, textCityB, path);
-						//result.setString(to_string(_graph->BellmanFord(textCityA, textCityB, path)));
-						result.setString(ans);
-						
-						//cout << _graph->BellmanFord("Orlando FL", "Lansing MI", path);
+						//resultBellman.setString(to_string(_graph->BellmanFord(textCityA, textCityB, path)));
+						resultBellman.setString(ansBellman);
+						resultDijkstra.setString(ansDijkstra);
+						//cout << _graph->BellmanFord("Orlando FL", "LansBellmaning MI", path);
 
 						
 
-						//result.setString(to_string(_graph->dijkstra(textCityA, textCityB, path)));
+						//resultBellman.setString(to_string(_graph->dijkstra(textCityA, textCityB, path)));
 					}
 					
 
@@ -251,7 +289,8 @@ int main()
 			textbox1.drawTo(window);
 			textbox2.drawTo(window);
 			calcBtn.drawTo(window);
-			window.draw(result);
+			window.draw(resultBellman);
+			window.draw(resultDijkstra);
 			window.display();
 
 		}		
